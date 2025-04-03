cdd229736e78fd93e2119725bf725513119e94b7
TheBookKnight <cadav001@umn.edu>
2025-04-03 12:18:24 -0400

    7 - Update jsdoc for tile param

--- src/components/tile.jsx
+++ src/components/tile.jsx
@@ -9,9 +9,10 @@ import './Tile.css'; // Reuse the same CSS file for tile styles
  * @param {function} props.onClick - The function to call when the tile is clicked.
  * @param {string} props.backgroundImage - The URL of the background image for the tile.
  * @param {string} props.backgroundPosition - The position of the background image for the tile.
+ * @param {number} props.gridSize - The size of the grid it'll used on (e.g., 5 for a 5x5 grid).
  * @returns {JSX.Element} The rendered Tile component.
  */
-const Tile = ({ flipped, onClick, backgroundImage, gridSize, backgroundPosition }) => {
+const Tile = ({ flipped, onClick, backgroundImage, backgroundPosition, gridSize }) => {
   return (
     <div className={`tile ${flipped ? 'flipped' : ''}`} onClick={onClick}>
       <div className="tile-inner">
